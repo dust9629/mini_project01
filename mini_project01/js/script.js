b22025d3a7ee07f68c4d9b871c7433006f242c15
Lindsey <lindsey@Lindseyui-MacBookPro.local>
2024-01-30 22:44:56 +0900

    modify error & connect img files

--- mini_project01/js/script.js
+++ mini_project01/js/script.js
@@ -1,9 +1,9 @@
-const sortableList = document.querySelectorAll(".sortable-list")
+const sortableList = document.querySelector(".sortable-list");
 const items = document.querySelectorAll(".item");
 
-items.forEach( item => {
+// 드래그 시작, 종료시 이벤트
+items.forEach(item => {
     item.addEventListener('dragstart', () => {
-        //item.classList.add('dargging');
         setTimeout(() => item.classList.add("dragging"), 0);
     });
     item.addEventListener('dragend', () => {
@@ -11,10 +11,12 @@ items.forEach( item => {
     });
 });
 
+// 드래그 리스트 초기화
 const initSortableList = (e) => {
     e.preventDefault();
-    const draggingItem = sortableList.querySelector(".dragging");
 
+    // 드래깅 되는 객체의 형제 선택자 가져오기
+    const draggingItem = sortableList.querySelector(".dragging");
     const siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
 
     let nextSibling = siblings.find(sibling => {
@@ -24,5 +26,7 @@ const initSortableList = (e) => {
     sortableList.insertBefore(draggingItem, nextSibling);
 }
 
+// 정렬된 리스트에 드래그 엔터
 sortableList.addEventListener("dragover", initSortableList);
-sortableList.addEventListener("dragender", e => e.preventDefault());
+// 드래그 엔터 이벤트 기본 동작 방지
+sortableList.addEventListener("dragenter", e => e.preventDefault());
